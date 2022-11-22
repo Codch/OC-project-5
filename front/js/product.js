c715d1055db643dc28e3dd9286900d6380acf618
Corentin "Codch" Petit <corentin.cpetit@gmail.com>
2022-11-22 17:50:15 +0100

    Récupérer l'API du produit

--- front/js/product.js
+++ front/js/product.js
@@ -5,9 +5,23 @@ var url = new URL(getPageURL);
 var id = url.searchParams.get("id");
 console.log(id);
 
-fetch("http://localhost:3000/api/products/"+id)
+function getElements() {
+    fetch("http://localhost:3000/api/products/"+id)
     .then(function(res) {
         console.log("succes", res);
+        return res.json();
     })
 
-    
+    .catch(function(error){
+        console.log("error", error);
+
+    })
+
+    .then(function(productAPI) {
+        const elements = productAPI;
+        console.log(elements);
+
+    })
+}
+
+getElements();
